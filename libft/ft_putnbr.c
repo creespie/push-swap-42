cd8810721dbeccbc378975922743fa5846e0f9cb
lorenzo mezzabarba <mezzabarba.lorenzo@gmail.com>
2026-05-26 23:57:27 +0200

    fixing

--- libft/ft_putnbr.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-void	ft_putnbr(int n)
-{
-	char	*number;
-
-	number = ft_itoa(n);
-	ft_putstr(number);
-}
--- libft/ft_putnbr_fd.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-void	ft_putnbr_fd(int n, int fd)
-{
-	char	*number;
-
-	number = ft_itoa(n);
-	ft_putstr_fd(number, fd);
-}
